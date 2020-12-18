1d3d29f560372e8ed09ba99427c3574a5108623c
Rutuj Bhakre <bhakrerutuj@gmail.com>
2020-12-18 19:34:11 +0530

    comments

--- blog/app/Http/Controllers/CommentController.php
+++ blog/app/Http/Controllers/CommentController.php
@@ -13,9 +13,9 @@ class CommentController extends Controller
    		$comment->comment = $request->comment;
    		$comment->post_id=$id;
    		$comment->user_id=auth()->user()->id;
-   		// echo $request->user_id;
-   		// echo $comment->comment;
-   		// echo $comment->post_id;
+   		echo $request->user_id;
+   		echo $comment->comment;
+   		echo $comment->post_id;
    		$comment->save();
    		return redirect()->route('post.single',[$id]);
    	}
